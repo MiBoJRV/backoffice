224f7703e43422b3dc151e057b91cd4160a8d7f0
Mykhailo Bodnar <mykhailo.bodnar@wetelo.com>
2023-11-14 17:54:05 +0100

    fixed redirect

--- src/layouts/AdminLayout/AdminLayout.jsx
+++ src/layouts/AdminLayout/AdminLayout.jsx
@@ -1,20 +1,72 @@
-import React from 'react';
-import {Sidebar} from "../../components/Sidebar/Sidebar.jsx";
-import {AdminHeader} from "../../components/AdminHeader/AdminHeader.jsx";
-import {AdminLayoutContent} from "./Styles.jsx";
-import {Outlet} from "react-router-dom";
+// import React, { useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { Sidebar } from "../../components/Sidebar/Sidebar.jsx";
+// import { AdminHeader } from "../../components/AdminHeader/AdminHeader.jsx";
+// import { AdminLayoutContent } from "./Styles.jsx";
+// import { Outlet } from "react-router-dom";
+//
+// const AdminLayout = ({ children, setUserRole }) => {
+//     const accessToken = localStorage.getItem('accessToken');
+//     const navigate = useNavigate();
+//
+//     useEffect(() => {
+//         if (!accessToken) {
+//             navigate('/login');
+//         }
+//     }, [accessToken, navigate]);
+//
+//     return (
+//         accessToken ? (
+//             <AdminLayoutContent>
+//                 <AdminHeader />
+//                 <div className="page_content">
+//                     <Sidebar setUserRole={setUserRole} />
+//                     {children}
+//                     <Outlet />
+//                 </div>
+//             </AdminLayoutContent>
+//         ) : null
+//     );
+// };
+//
+// export default AdminLayout;
 
-const AdminLayout = ({children, setUserRole}) => {
+
+
+
+
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Sidebar } from "../../components/Sidebar/Sidebar.jsx";
+import {AdminLayoutContent} from "./Styles.jsx";
+import { Outlet } from "react-router-dom";
+import {AdminHeader} from "../../components/AdminHeader/AdminHeader.jsx";
+
+const CustomerLayout = ({ children, setUserRole }) => {
+    const accessToken = localStorage.getItem('accessToken');
+    const userRole = localStorage.getItem('userRole');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!accessToken) {
+            navigate('/login');
+        } else if (accessToken && userRole !== "Admin") {
+            navigate('/customer/dashboard');
+        }
+    }, [accessToken, userRole, navigate]);
+
     return (
-        <AdminLayoutContent>
-            <AdminHeader/>
-            <div className="page_content">
-                <Sidebar setUserRole={setUserRole}/>
-                {children}
-                <Outlet />
-            </div>
-        </AdminLayoutContent>
+        (accessToken && userRole === "Admin") ? (
+            <AdminLayoutContent>
+                <AdminHeader />
+                <div className="page_content">
+                    <Sidebar setUserRole={setUserRole} />
+                    {children}
+                    <Outlet />
+                </div>
+            </AdminLayoutContent>
+        ) : null
     );
 };
 
-export default AdminLayout;
+export default CustomerLayout;
--- src/layouts/CustomerLayout/CustomerLayout.jsx
+++ src/layouts/CustomerLayout/CustomerLayout.jsx
@@ -1,22 +1,102 @@
-import React from "react";
+// import React from "react";
+// import { Sidebar } from "../../components/Sidebar/Sidebar.jsx";
+// import { User } from "../../components/User/User.jsx";
+// import {CustomerLayoutContent} from "./Styles.jsx";
+// import useCustomerData from "../../hooks/useCustomerData.jsx";
+// import {Outlet} from "react-router-dom";
+//
+// const CustomerLayout = ({ children, setUserRole }) => {
+//     const { customerData, error } = useCustomerData();
+//
+//     return (
+//         <CustomerLayoutContent>
+//             <User {...customerData} />
+//             <div className="page_content">
+//                 <Sidebar setUserRole={setUserRole} />
+//                 {children}
+//                 <Outlet />
+//             </div>
+//         </CustomerLayoutContent>
+//     );
+// };
+//
+// export default CustomerLayout;
+
+// import React, { useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { Sidebar } from "../../components/Sidebar/Sidebar.jsx";
+// import { CustomerLayoutContent } from "./Styles.jsx";
+// import { Outlet } from "react-router-dom";
+// import { User } from "../../components/User/User.jsx";
+// import useCustomerData from "../../hooks/useCustomerData.jsx";
+//
+// const CustomerLayout = ({ children, setUserRole }) => {
+//     const accessToken = localStorage.getItem('accessToken');
+//     const userRole = localStorage.getItem('userRole');
+//     const navigate = useNavigate();
+//     const { customerData, error } = useCustomerData();
+//
+//     useEffect(() => {
+//         if (!accessToken) {
+//             navigate('/login');
+//         } else if (accessToken && userRole !== "Customer") {
+//             navigate('/admin/accounts');
+//         }
+//     }, [accessToken, userRole, navigate]);
+//
+//     return (
+//         (accessToken && userRole === "Customer") ? (
+//             <CustomerLayoutContent>
+//                 <User {...customerData} />
+//                 <div className="page_content">
+//                     <Sidebar setUserRole={setUserRole} />
+//                     {children}
+//                     <Outlet />
+//                 </div>
+//             </CustomerLayoutContent>
+//         ) : null
+//     );
+// };
+//
+// export default CustomerLayout;
+
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Sidebar } from "../../components/Sidebar/Sidebar.jsx";
+import { CustomerLayoutContent } from "./Styles.jsx";
+import { Outlet } from "react-router-dom";
 import { User } from "../../components/User/User.jsx";
-import {CustomerLayoutContent} from "./Styles.jsx";
 import useCustomerData from "../../hooks/useCustomerData.jsx";
-import {Outlet} from "react-router-dom";
 
 const CustomerLayout = ({ children, setUserRole }) => {
-    const { customerData, error } = useCustomerData();
+    const accessToken = localStorage.getItem('accessToken');
+    const userRole = localStorage.getItem('userRole');
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!accessToken) {
+            navigate('/login');
+        } else if (accessToken && userRole !== "Customer") {
+            navigate('/admin/accounts');
+        }
+    }, [accessToken, userRole, navigate]);
+
+    // Fetch customerData only when conditions are met
+    const customerData = (accessToken && userRole === "Customer") ? useCustomerData().customerData : null;
+
     return (
-        <CustomerLayoutContent>
-            <User {...customerData} />
-            <div className="page_content">
-                <Sidebar setUserRole={setUserRole} />
-                {children}
-                <Outlet />
-            </div>
-        </CustomerLayoutContent>
+        (accessToken && userRole === "Customer") ? (
+            <CustomerLayoutContent>
+                {customerData && <User {...customerData} />}
+                <div className="page_content">
+                    <Sidebar setUserRole={setUserRole} />
+                    {children}
+                    <Outlet />
+                </div>
+            </CustomerLayoutContent>
+        ) : null
     );
 };
 
 export default CustomerLayout;
+
